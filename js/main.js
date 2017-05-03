87a148a1bb913c2f6bb54aad1d2cd9da51f5bc42
humdrum <jjrajani@gmail.com>
2017-05-03 19:23:32 -0400

    updated into text. added functions to main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,21 @@
 console.log("Hello World");
+
+var add = function(a, b) {
+  return a + b;
+}
+
+let four = add(2, 2);
+console.log("four");
+console.log(add(2, 2));
+console.log(four);
+
+var multiply = function (a, b) {
+  return a * b;
+}
+
+let six = multiply(2, 3);
+console.log("six");
+console.log(multiply(2, 3));
+console.log(six);
+
+// make functions for subtracting and dividing.
